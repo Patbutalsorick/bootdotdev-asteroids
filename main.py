ea85d09cb3f12ca063fe00e59b65977b5b9aed4f
Patrick Garcha <pat@Patricks-MacBook-Pro.local>
2026-08-29 22:50:12 +1000

    Added forward and backward movement for player

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(
+        (f"Starting Asteroids with pygame version: {pygame.version.ver}\n"),
+        (f"Screen width: {SCREEN_WIDTH}\n"),
+        (f"Screen height: {SCREEN_HEIGHT}"),
+    )
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -15,8 +24,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
